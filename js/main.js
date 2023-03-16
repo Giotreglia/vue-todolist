75a5aacd2958273aea1a4128d8f4ece74a7784ea
Giotreglia <giovanni.treglia@gmail.com>
2023-03-16 16:27:10 +0100

    Milestone 1

--- js/main.js
+++ js/main.js
@@ -3,7 +3,36 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        tasks: [
+            {
+                text: 'Creare repository vue-todolist',
+                done: true
+            },
+            {
+                text: 'MILESTONE 1',
+                done: false
+            },
+            {
+                text: 'MILESTONE 2',
+                done: false
+            },
+            {
+                text: 'MILESTONE 3',
+                done: false
+            },
+            {
+                text: 'Bonus 1',
+                done: false
+            },
+            {
+                text: 'Bonus 2',
+                done: true
+            },
+            {
+                text: 'Ultimare esercizio',
+                done: true
+            },
+        ]
       }
     }
   }).mount('#app')
